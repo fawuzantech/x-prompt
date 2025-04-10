6043334f240148d791dcce32499c6fe84ee264e9
fawuzantech <ibrahimpima76@gmail.com>
2025-04-10 15:15:25 +0000

    init

---
+++ app/components/footer.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <h2>Footer</h2>
+    </div>
+  )
+}
---
+++ app/components/landing.tsx
@@ -0,0 +1,14 @@
+"use client"
+import Hero from "@/app/components/hero"
+import { Footer } from "@/app/components/footer"
+export const Landing = () => {
+ return(
+    <div>
+      <Hero/>
+   <div>
+<Footer/>
+</div>
+   </div>
+   )
+
+}
