3192ce3528a4ae6dfc66468a2d18480c5d0780b9
fawuzantech <ibrahimpima76@gmail.com>
2025-04-10 21:55:01 +0000

    added footer

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -55,7 +55,7 @@ export default function Hero() {
                onClick={GoToPrompt}
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                create now
+                start now
               </button>
               <a href="#" className="text-sm/6 font-semibold text-gray-900">
                 {/* Learn more <span aria-hidden="true">→</span> */}
